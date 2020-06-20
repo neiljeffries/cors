e0476c84c062141d15031215b485b85ad7e399f6
neiljeffries <neiljeffries@gmail.com>
2020-06-20 14:24:10 -0400

    upds

--- src/main/java/com/example/restservicecors/GreetingController.java
+++ src/main/java/com/example/restservicecors/GreetingController.java
@@ -1,41 +1,40 @@
 package com.example.restservicecors;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-	public class Fizzle{
-		private List<String> foo;
-		private boolean bar;
-		private int baz;
-		// getters and setters omitted
-	}
+	// public class Fizzle{
+	// 	private List<String> foo;
+	// 	private boolean bar;
+	// 	private int baz;
+	// 	// getters and setters omitted
+	// }
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 
 	@GetMapping("/greeting-javaconfig")
-	public Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+	public ResponseEntity<Greeting> greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
 		System.out.println("==== in greeting ====");
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return ResponseEntity.ok().body( new Greeting(counter.incrementAndGet(), String.format(template, name)) );
 	}
 
 	@RequestMapping(value = "/process",	method = RequestMethod.POST)
-	public void process(@RequestBody Map<String, Object> payload) throws Exception {
+	public ResponseEntity<Object> process(@RequestBody Map<String, Object> payload) throws Exception {
 	  System.out.println(payload);
+	  return ResponseEntity.ok().body(payload);
 	}
 
 }
